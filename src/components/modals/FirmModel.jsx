0512cd547f6d7552b712e62d8831e70df7c1e719
Kemal <i.kemal.karabulut@gmail.com>
2023-01-01 12:59:50 +0100

    kpi and charts added

--- src/components/modals/FirmModel.jsx
+++ src/components/modals/FirmModel.jsx
@@ -1,31 +1,31 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
-import { flexCenter, modalStyle } from "../../styles/globalStyle";
-import { TextField } from "@mui/material";
+import { flexColumn, modalStyle } from "../../styles/globalStyle";
+import { Button, TextField } from "@mui/material";
 import useStockCalls from "../../hooks/useStockCalls";
 
 export default function FirmModal({ open, setOpen, info, setInfo }) {
-  const { PostFirm, PutFirm } = useStockCalls();
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setInfo({ ...info, [name]: value });
-  }
-  function handleSubmit(e) {
+  const { postFirm, putFirm } = useStockCalls();
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (info.id) {
-      PutFirm(info);
-      console.log("put firm clicked");
+      putFirm(info);
     } else {
-      console.log("POSTED firm clicked");
-      PostFirm(info);
+      postFirm(info);
     }
     setOpen(false);
     setInfo({});
-  }
+  };
 
-  const { name, phone, address, image } = info;
+  console.log(info);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInfo({ ...info, [name]: value });
+  };
+
   return (
     <div>
       <Modal
@@ -38,45 +38,52 @@ export default function FirmModal({ open, setOpen, info, setInfo }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={modalStyle}>
-          <Box sx={flexCenter} component="form" onSubmit={handleSubmit}>
+          <Box component="form" onSubmit={handleSubmit} sx={flexColumn}>
             <TextField
               label="Firm Name"
               name="name"
               id="name"
               type="text"
               variant="outlined"
-              value={name || ""}
+              required
+              value={info?.name || ""}
               onChange={handleChange}
             />
+
             <TextField
-              label="Phone Number"
+              label="Phone"
               name="phone"
               id="phone"
-              type="text"
+              type="tel"
+              required
               variant="outlined"
-              value={phone || ""}
+              value={info?.phone || ""}
               onChange={handleChange}
             />
+
             <TextField
               label="Address"
               name="address"
               id="address"
               type="text"
+              required
               variant="outlined"
-              value={address || ""}
+              value={info?.address || ""}
               onChange={handleChange}
             />
+
             <TextField
-              label="Image URL"
+              label="Image"
               name="image"
               id="image"
               type="url"
+              required
               variant="outlined"
-              value={image || ""}
+              value={info?.image || ""}
               onChange={handleChange}
             />
             <Button type="submit" variant="contained" size="large">
-              {info?.id ? "Update Firm" : "Save Firm"}
+              Submit Firm
             </Button>
           </Box>
         </Box>
